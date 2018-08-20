d4e17f5f2eecbd5795ad50eef03beb537397f9b6
sbrendel <illnesse@gmail.com>
2018-08-20 21:34:18 +0200

    latency ui

--- transform/latencyUI.js
+++ transform/latencyUI.js
@@ -1,5 +1,6 @@
 //    if ((i == "-") || (i == "0") || (i == "UNDEF") || (i == null) || (i == "")) return "OFFLINE";
 (function(i) {
     if ((i == "-") || (i == "UNDEF") || (i == null) || (i == "")) return "OFFLINE";
-    return "ONLINE "+i+" ms";
+    return i+" ms";
+    //return "ONLINE "+i+" ms";
 })(input)
