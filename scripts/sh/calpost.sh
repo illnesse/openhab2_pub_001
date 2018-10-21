8693b602ee642607fce208b8b22ccf3316cec221
sbrendel <illnesse@gmail.com>
2018-10-21 21:17:41 +0200

    missed a bunch of stuff

--- scripts/sh/calpost.sh
+++ scripts/sh/calpost.sh
@@ -1,2 +1,3 @@
 #echo $@
-/usr/bin/python /etc/openhab2/scripts/python/CalSyncHAB/CalSyncHAB.py --noauth_local_webserver --id=$1 --action=$2 --summary="$3" --begin=$4 --end=$5
+cd /etc/openhab2/scripts/python/gcal
+/usr/bin/python /etc/openhab2/scripts/python/gcal/syncgcal.py --noauth_local_webserver --id=$1 --action=$2 --summary="$3" --begin=$4 --end=$5
